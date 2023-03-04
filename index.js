1be38da327e7c47b17efb2e4aa3b750572db46f0
สหสวรรษ อึ้งวณิชย์พันธ์ <>
2023-03-04 18:19:34 +0700

    hok

---
+++ index.js
@@ -0,0 +1,82 @@
+window.addEventListener('scroll', function () {
+  const page1 = this.document.getElementById('sw-page')
+  const page2 = this.document.getElementById('htu-page')
+  const page3 = this.document.getElementById('n-page')
+  const items = document.getElementsByClassName('fev-item')
+
+  if (this.scrollY - page1.clientHeight < 0) {
+    document.getElementsByClassName('mobile-title')[0].innerHTML = "Save Wash"
+    items[0].classList.add('selected');
+    items[1].classList.remove('selected');
+    items[2].classList.remove('selected');
+    items[3].classList.remove('selected');
+    items[4].classList.add('selected');
+    items[5].classList.remove('selected');
+    items[6].classList.remove('selected');
+    items[7].classList.remove('selected');
+  }
+  else if (this.scrollY - page1.clientHeight >= 0 && this.scrollY - (page2.clientHeight + page1.clientHeight) < 0) {
+    document.getElementsByClassName('mobile-title')[0].innerHTML = "บริการและผลิตภัณฑ์"
+    items[0].classList.remove('selected');
+    items[1].classList.add('selected');
+    items[2].classList.remove('selected');
+    items[3].classList.remove('selected');
+    items[4].classList.remove('selected');
+    items[5].classList.add('selected');
+    items[6].classList.remove('selected');
+    items[7].classList.remove('selected');
+  }
+  else if (this.scrollY - page1.clientHeight >= 0 && this.scrollY - (page2.clientHeight + page1.clientHeight) >= 0 && this.scrollY - (page3.clientHeight + page2.clientHeight + page1.clientHeight) < 0) {
+    document.getElementsByClassName('mobile-title')[0].innerHTML = "การเดินทาง"
+    items[0].classList.remove('selected');
+    items[1].classList.remove('selected');
+    items[2].classList.add('selected');
+    items[3].classList.remove('selected');
+    items[4].classList.remove('selected');
+    items[5].classList.remove('selected');
+    items[6].classList.add('selected');
+    items[7].classList.remove('selected');
+  }
+  else {
+    document.getElementsByClassName('mobile-title')[0].innerHTML = "ติดต่อ"
+    items[0].classList.remove('selected');
+    items[1].classList.remove('selected');
+    items[2].classList.remove('selected');
+    items[3].classList.add('selected');
+    items[4].classList.remove('selected');
+    items[5].classList.remove('selected');
+    items[6].classList.remove('selected');
+    items[7].classList.add('selected');
+  }
+})
+
+function closeSideBar() {
+  const open_btn = document.getElementsByClassName('close')
+  const sidebar = document.getElementsByClassName('sidebar')
+  open_btn[0].style.display = 'flex'
+  document.documentElement.style.overflowY = 'auto'
+  sidebar[0].style.width = '0'
+}
+
+function openSideBar() {
+  const open_btn = document.getElementsByClassName('close')
+  const sidebar = document.getElementsByClassName('sidebar')
+  open_btn[0].style.display = 'none'
+  document.documentElement.style.overflowY = 'hidden'
+  sidebar[0].style.width = '100vw'
+}
+
+function expandPanel(i) {
+  const items = document.getElementsByClassName('fev-item')
+  for (let index = 0; index < items.length; index++) {
+    const item = items[index];
+    if (i === 0 || i === 4) document.getElementById('sw-page').scrollIntoView()
+    if (i === 1 || i === 5) document.getElementById('htu-page').scrollIntoView()
+    if (i === 2 || i === 6) document.getElementById('n-page').scrollIntoView()
+    if (i === 3 || i === 7) document.getElementById('c-page').scrollIntoView()
+    if (i < 4) document.getElementsByClassName('mobile-title')[0].innerHTML = items[i].innerHTML
+    if (i === index) item.classList.add('selected')
+    else item.classList.remove('selected')
+  }
+  if (i < 4) closeSideBar()
+}
